68fe6070884a504470993889b802aac885d8ca9e
Turan <90398677+Turan141@users.noreply.github.com>
2023-12-25 20:22:08 +0400

    Add files via upload

---
+++ src/assets/icons/Request.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+
+export const Request = () => {
+	return (
+		<svg
+			width='56'
+			height='56'
+			viewBox='0 0 56 56'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<path
+				fill-rule='evenodd'
+				clip-rule='evenodd'
+				d='M3.33897e-07 28C5.18303e-07 43.464 12.536 56 28 56C43.464 56 56 43.464 56 28C56 12.536 43.464 1.49491e-07 28 3.33897e-07C12.536 5.18303e-07 1.49491e-07 12.536 3.33897e-07 28Z'
+				fill='#222730'
+			/>
+			<g clip-path='url(#clip0_2_485)'>
+				<path
+					fill-rule='evenodd'
+					clip-rule='evenodd'
+					d='M39.5301 23.0604L34.5301 28.0604L33.4694 26.9998L37.1889 23.2803H16.9998V21.7803H37.1893L33.4694 18.0604L34.5301 16.9998L39.5301 21.9998L40.0604 22.5301L39.5301 23.0604Z'
+					fill='#F7F9FA'
+				/>
+				<path
+					fill-rule='evenodd'
+					clip-rule='evenodd'
+					d='M16.5301 32.9979L21.5301 27.9979L22.5907 29.0586L18.8712 32.7781H39.0604V34.2781H18.8709L22.5907 37.9979L21.5301 39.0586L16.5301 34.0586L15.9998 33.5283L16.5301 32.9979Z'
+					fill='#F7F9FA'
+				/>
+			</g>
+			<defs>
+				<clipPath id='clip0_2_485'>
+					<rect width='24' height='24' fill='white' transform='translate(15.9998 16)' />
+				</clipPath>
+			</defs>
+		</svg>
+	)
+}
